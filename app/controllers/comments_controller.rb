c81ae06c0482796513e223dcdfb13d947234b65a
tatsuya-koyam26 <koyama.tatsuya@icloud.com>
2019-08-13 21:17:31 +0900

    微調整

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    # @comment = @post.comments.create(params[:comment])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
