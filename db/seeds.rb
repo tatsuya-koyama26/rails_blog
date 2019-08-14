c669c6e4bb9fdac9e15f44fac5735b68eb0ec446
tatsuya-koyam26 <koyama.tatsuya@icloud.com>
2019-08-14 12:46:54 +0900

    seed使用

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |i|
+  Post.create(
+          name: "名前 #{i}",
+          title: "タイトル #{i}",
+          content: "コンテンツ #{i}"
+  )
+end
